2afd5a881d8537d178be83ab29f903b4c2ffee8c
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-01-25 20:05:59 +0530

    updat

---
+++ js/day 1/script.js
@@ -0,0 +1,32 @@
+// variables 
+
+
+// automatic
+// var 
+// let 
+// const 
+
+
+// x = 5;
+// y = 6;
+// z = y+x;
+
+
+// var x = 5;
+// var y = 6;
+// var z = x+y;
+
+
+// let and const ;
+
+
+x = 45;
+y = 656;
+
+
+
+
+
+
+
+
---
+++ js/day 2/script.js
@@ -0,0 +1,204 @@
+// let x = 5;
+// let y = 6;
+
+
+// let z = x+y;
+
+
+// console.log(z);
+
+
+
+// const x = 5;
+// {
+//     let x = 10;
+// }
+
+
+// var arun56_$= 34;
+
+// var a56asasd=78;
+//  let $asht = 56;
+//  let _asht = 56;
+
+// let Arun = 56;
+// let arun =56;
+
+// x = 10;
+// y = x+5
+
+// x == y ;
+
+
+// Data types 
+
+
+// const pi = 3.14;
+
+// let person = "Arun Raj";
+// let answer = 'yes i am! ar';
+
+// var carName;
+
+// let carName;
+
+// const carName = 'Toya';
+
+
+
+// let
+// // block scoped
+// let must be declared before use 
+//cannot be redeclared in the same scope
+
+
+// {
+//     let x = 2;
+//     console.log(x);
+// }
+
+
+// {
+//     var x =5;
+
+// }
+
+// console.log(x);
+
+
+// let x = "John";
+
+//     let x = 0;
+
+
+// var x = "john";
+
+// var x = 56;
+
+// console.log(x);
+
+
+
+
+// var x = 10 ;
+// console.log(x);
+
+// {
+//     var x = 2;
+// console.log(x);
+// }
+
+// console.log(x);
+
+//  let x = 56;
+
+
+//  {
+//     let x = 2;
+//     console.log(x);
+
+//  }
+
+// console.log(x);
+
+
+// carName = "volvo"
+// var carName;
+
+
+
+
+// const 
+
+// cannot be redeclared 
+// cannot be reassigned
+// block scoped
+
+// const PI = 3.145454696564;
+
+// PI = 3.15;  // this is typeerror
+
+// PI = PI + 10; // this is also type error
+
+// console.log(PI);
+
+
+// const PI = 3.145486945;
+
+
+
+// useage of const
+
+// a new array 
+// a new object 
+// a new function 
+// new RegEXp
+
+
+// const Car = ["arun", "volvo", "BMW"];
+
+
+// Car [0]= "toyoota";
+
+// Car.push("Audi");
+
+
+// console.log(Car);
+
+
+// const Car = { 
+//     type: "Fiat", 
+//     model:"500" , 
+//     color:"red"
+// };
+
+// Car.color= "yellow";
+
+
+// Car.owner = "Arun";
+// console.log(Car);
+
+
+
+
+
+
+
+
+
+
+
+0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
