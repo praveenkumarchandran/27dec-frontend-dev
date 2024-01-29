e828c61d28d09c23f7a0b662d33a1e0149d4fd5d
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-01-29 20:03:58 +0530

    day 3 js

---
+++ js/day 3/datatype.js
@@ -0,0 +1,88 @@
+//string 
+
+// var instute =  'besant tech';
+
+
+//number 
+
+
+// var profit = 500;
+// var loss = 100;
+
+// total = profit- loss;
+
+
+// console.log(total);
+
+
+// let x = 5 ;
+// let y = 5;
+// let z = 6;
+
+
+// console.log(x==y);
+
+// console.log(x == z);
+
+
+// array 
+
+
+// const cars = ["arun", "volvo", "BMW"];
+
+// cars[0]= "BENZ";
+
+// console.log(cars);
+
+
+
+// const person = {
+//     firstName:"arun" ,
+//     lastName :"raj",
+//     age :50,
+//     eyeColor:"blue",
+// }
+
+// person.age=23;
+
+// console.log(person);
+
+
+
+// const number = [
+//     [1,2,3], //0
+//     4, //1
+//     [5,[6,7]],//[2][1][1]
+//     8,//3
+//     [9,10,[11,[12]]],//4 2 0
+// ];
+
+
+
+// console.log(number[1]);
+// console.log(number[2[1[1]]]);
+
+// console.log(number [2][1][1]);
+// console.log(number [4][2][1][0]);
+
+
+// console.log(number.length);
+
+// console.log(number);
+
+// console.log(number.toString());
+
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+
+// const myChildren = myGirls.concat(myBoys);
+
+// console.log(myChildren);
+
+
+
+
+
+
+
