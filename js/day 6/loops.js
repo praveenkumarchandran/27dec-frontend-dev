4e1f54448e3889a75ddb68198f6069ebfff43427
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-02-01 20:19:52 +0530

    update js day 6

---
+++ js/day 6/loops.js
@@ -0,0 +1,115 @@
+// for loop , while loop  do while looop
+
+// syntax 
+
+// for (inzlization; condition; increment){
+//     //..code to be exicuted 
+// }
+
+// for (let i = 1 ; i <= 10 ; i++){
+//     console.log(i);
+// }
+
+
+// var sum = 0 ;
+// for (var i = 1 ; i <=50; i++){
+//     sum = sum +i ;
+// }
+
+// console.log("sum ="+ sum);
+
+// for in
+
+
+// var student = { name:'arun', age:25 , degree:"masters"};
+
+
+// for (var item in student){
+//     console.log(student[item]);
+// }
+
+
+
+
+
+// while loop
+
+// syntax 
+
+// while (condition){
+//     //code to be exicuted 
+//     // incremnt
+// }
+
+
+// let i = 1;
+
+// while (i < 10){
+//     console.log(i);
+//     i++
+// }
+
+
+
+// do while loop 
+// syntax
+
+// do {
+//     //code to be exicuted 
+// } while (condiion);
+
+
+
+// let i = 1 ;
+// do {
+//     i++;
+//     console.log(i);
+// } while  (i<=0);
+
+
+// do while 
+
+
+// var sum = 0;
+// var number = 1;
+
+
+// do {
+//     sum += number;
+//     number++;
+// }while (number <= 50)
+
+// console.log("sum = "+ sum );
+
+
+
+// do {
+//     //read a character from a keyboard in the body
+// } while (if ch === "0") 
+
+// break 
+
+// var sum = 0 ;
+
+
+// for (var i = 1; i < 20; i++){
+//     sum += i ;
+//     if (i === 5){
+//        break;// tranfres control outside the for block
+//     }
+// }
+
+// console.log("sum =" +sum);
+
+
+// for (var i = 1; i <= 10; i++){
+//     if ((i % 2) != 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
