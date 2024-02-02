d4d4403e79565912a973c7a9385b2c236c7de517
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-02-02 20:07:56 +0530

    day 7

--- js/day 7/date.js
+++ js/day 7/date.js
@@ -4,5 +4,43 @@ console.log("current date is :",currentDate);
 
 
 let year = currentDate.getFullYear();
+let month = currentDate.getMonth() + 1;
+let day = currentDate.getDate();
+
+console.log("year:", year);
+console.log("Day:", day);
+console.log("month:",month);
+
+
+currentDate.setFullYear(2005);
+
+currentDate.setMonth(5);
+currentDate.setDate(15);
+
+console.log("updated", currentDate);
+
+
+let dayOfWeek = currentDate.getDay("2024-02-02");
+console.log("day of the week : ", dayOfWeek);
+
+
+
+let formatteddate = currentDate.toDateString();
+console.log("formatted date:",formatteddate);
+
+
+currentDate.setDate(currentDate.getDate()+5);
+currentDate.setHours(currentDate.getHours()-3);
+
+
+console.log("udated date after ading 5 days and subract 3 hrs:", currentDate);
+
+
+let dateString = '2024-02-15';
+
+let parseDate = new Date(dateString);
+
+
+console.log("parse date form string:",parseDate);
 
 
